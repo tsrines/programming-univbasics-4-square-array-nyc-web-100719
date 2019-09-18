504bfd553b44bb94a96032ca0013106647830e53
Timothy Rines <tsrines@gmail.com>
2019-09-18 21:01:20 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,7 +4,7 @@ def square_array(array)
   counter = 0
   while counter < array.length do
     new_numbers << (array[counter] ** 2)
-    counter = count + 1
+    counter = counter + 1
     end
     new_numbers
 end
